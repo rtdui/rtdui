0deca9a455d50a09ae8b049f2c585050786e727a
fuzhongwei <jackyleefzw@163.com>
2023-12-21 15:29:25 +0800

    [docs_vite] Fix typing

--- docs_vite/src/demos/link/LinkPolymorphicDemo.tsx
+++ docs_vite/src/demos/link/LinkPolymorphicDemo.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import { Link as RouterLink, NavLink } from "react-router-dom";
 import { Link } from "@rtdui/core";
 
-export default function Demo() {
+export default function LinkBasicDemo() {
   return (
     <div className="flex gap-4 items-center">
       <Link as="a" href="/">
         native anchor
       </Link>
       <Link as={RouterLink} to="/">
-        Remix Link
+        react-router Link
       </Link>
       <Link as={NavLink} to="/components/link">
-        Remix NavLink
+        react-router NavLink
       </Link>
     </div>
   );
 }
-Demo.displayName = "LinkPolymorphicDemo";
