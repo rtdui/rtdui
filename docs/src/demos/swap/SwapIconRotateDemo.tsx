accccc09c6852994f137e34edde4f3f31eea05f8
fuzhongwei <jackyleefzw@163.com>
2023-12-28 16:20:18 +0800

    [docs] SwapIconRotateDemo add gap

--- docs/src/demos/swap/SwapIconRotateDemo.tsx
+++ docs/src/demos/swap/SwapIconRotateDemo.tsx
@@ -3,7 +3,7 @@ import { IconMoon, IconSun, IconMenu2, IconX } from "@tabler/icons-react";
 
 export default function Demo() {
   return (
-    <>
+    <div className="flex gap-8">
       <Swap transitionEffect="rotate">
         <IconMoon size={36} />
         <IconSun size={36} />
@@ -12,7 +12,7 @@ export default function Demo() {
         <IconMenu2 size={36} />
         <IconX size={36} />
       </Swap>
-    </>
+    </div>
   );
 }
 Demo.displayName = "SwapIconRotateDemo";
