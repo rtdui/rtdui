ce248e0375591c873f971bd1e027178c9983f6ad
fuzhongwei <jackyleefzw@163.com>
2024-01-03 12:21:17 +0800

    [docs] add datatable selection, pagination, tree demos

---
+++ docs/src/demos/datatable_pagination/DataTablePaginationDemo.tsx
@@ -0,0 +1,52 @@
+import type { ColumnDef } from "@tanstack/react-table";
+import React from "react";
+import { DataTable, DataTableProps } from "@rtdui/datatable";
+import { makeData, type Person } from "../../demoData/makeData";
+
+const columns: ColumnDef<Person>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    id: "姓名",
+    accessorFn: (row) => `${row.firstName}${row.lastName}`,
+  },
+  {
+    accessorKey: "age",
+    header: "年龄",
+  },
+];
+const tableProps: Partial<DataTableProps> = {
+  enableVirtualized: false,
+  enablePagination: true,
+  enableColumnReorder: true,
+  enableColumnResizing: true,
+  enableSorting: true,
+  enableGrouping: false,
+  enableFilters: true,
+  filterFromLeafRows: false,
+  enableHiding: true,
+  enableRowSelection: false,
+  enableMultiRowSelection: false,
+  enableSubRowSelection: false,
+  enableClickRowSelection: false,
+  selectAllForAllPages: false,
+  enableStickyHeader: true,
+  enableAutoRowNumber: false,
+  enableExport: true,
+};
+export default function Demo() {
+  const [data, setData] = React.useState<Person[]>([]);
+
+  React.useEffect(() => {
+    setData(makeData(50, 3, 2));
+  }, []);
+
+  return (
+    <div className="h-[600px]">
+      <DataTable data={data} columns={columns} {...tableProps} />
+    </div>
+  );
+}
+Demo.displayName = "DataTablePaginationDemo";
---
+++ docs/src/demos/datatable_selection/DataTableClickRowSelectionDemo.tsx
@@ -0,0 +1,68 @@
+import type { ColumnDef } from "@tanstack/react-table";
+import React from "react";
+import { DataTable, DataTableProps } from "@rtdui/datatable";
+import { makeData, type Person } from "../../demoData/makeData";
+
+const columns: ColumnDef<Person>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    id: "姓名",
+    accessorFn: (row) => `${row.firstName}${row.lastName}`,
+  },
+  {
+    accessorKey: "age",
+    header: "年龄",
+  },
+];
+const tableProps: Partial<DataTableProps> = {
+  enableVirtualized: false,
+  enablePagination: false,
+  enableColumnReorder: true,
+  enableColumnResizing: true,
+  enableSorting: true,
+  enableGrouping: false,
+  enableFilters: true,
+  filterFromLeafRows: false,
+  enableHiding: true,
+  enableRowSelection: true,
+  enableMultiRowSelection: false,
+  enableSubRowSelection: false,
+  enableClickRowSelection: true,
+  selectAllForAllPages: true,
+  enableStickyHeader: true,
+  enableAutoRowNumber: false,
+  enableExport: true,
+};
+export default function Demo() {
+  const [data, setData] = React.useState<Person[]>([]);
+
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  React.useEffect(() => {
+    setData(makeData(50));
+  }, []);
+
+  const ref = React.useRef<any>();
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="h-96">
+        <DataTable
+          ref={ref}
+          data={data}
+          columns={columns}
+          {...tableProps}
+          onRowSelectionChange={setRowSelection}
+          state={{
+            rowSelection,
+          }}
+        />
+      </div>
+      <output>当前的选择: {JSON.stringify(rowSelection, undefined, 2)}</output>
+    </div>
+  );
+}
+Demo.displayName = "DataTableClickRowSelectionDemo";
