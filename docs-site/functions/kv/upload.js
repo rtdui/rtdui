05262fbdf553f3d3e7ec23f6a6e4358e9be46117
fuzhongwei <jackyleefzw@163.com>
2024-03-28 10:37:55 +0800

    [docs-site] use parseMultipartFormData from @remix-run/server-runtime

--- docs-site/functions/kv/upload.js
+++ docs-site/functions/kv/upload.js
@@ -1,35 +1,6 @@
-import * as multipartParser from "@web3-storage/multipart-parser";
+import { unstable_parseMultipartFormData as parseMultipartFormData } from "@remix-run/server-runtime";
 
-/**
- * Allows you to handle multipart forms (file uploads) for your app.
- *
- * TODO: Update this comment
- * @see https://remix.run/utils/parse-multipart-form-data
- */
-async function parseMultipartFormData(request, uploadHandler) {
-  let contentType = request.headers.get("Content-Type") || "";
-  let [type, boundary] = contentType.split(/\s*;\s*boundary=/);
-  if (!request.body || !boundary || type !== "multipart/form-data") {
-    throw new TypeError("Could not parse content as FormData.");
-  }
-  let formData = new FormData();
-  let parts = multipartParser.streamMultipart(request.body, boundary);
-  for await (let part of parts) {
-    if (part.done) break;
-    if (typeof part.filename === "string") {
-      // only pass basename as the multipart/form-data spec recommends
-      // https://datatracker.ietf.org/doc/html/rfc7578#section-4.2
-      part.filename = part.filename.split(/[/\\]/).pop();
-    }
-    let value = await uploadHandler(part);
-    if (typeof value !== "undefined" && value !== null) {
-      formData.append(part.name, value);
-    }
-  }
-  return formData;
-}
-
-export function createCloudflareKvUploadHandler(options) {
+function createCloudflareKvUploadHandler(options) {
   const { kv, expirationTtl } = options ?? {};
   return async ({ filename, contentType, name, data }) => {
     // 将类型为AsyncIterable<Uint8Array>的data对象转换为Blob对象
