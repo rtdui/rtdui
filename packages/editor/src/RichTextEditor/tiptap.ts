c8c5044249211e3b5048873888324c7f8cc2a19d
fuzhongwei <jackyleefzw@163.com>
2024-03-28 08:19:16 +0800

    [@rtdui/editer] Commenting out unnecessary code

--- packages/editor/src/RichTextEditor/tiptap.ts
+++ packages/editor/src/RichTextEditor/tiptap.ts
@@ -12,11 +12,11 @@ import Placeholder from "@tiptap/extension-placeholder";
 import FloatingMenu from "@tiptap/extension-floating-menu";
 import TaskList from "@tiptap/extension-task-list";
 import TaskItem from "@tiptap/extension-task-item";
-import Collaboration from "@tiptap/extension-collaboration";
-import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
-import { HocuspocusProvider } from "@hocuspocus/provider";
-import * as Y from "yjs";
-import type { Doc } from "yjs";
+// import Collaboration from "@tiptap/extension-collaboration";
+// import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
+// import { HocuspocusProvider } from "@hocuspocus/provider";
+// import * as Y from "yjs";
+// import type { Doc } from "yjs";
 import { refractor } from "refractor/lib/all.js";
 // import { IndexeddbPersistence } from "y-indexeddb";
 // 自定义扩展
@@ -29,26 +29,26 @@ import {
 import { CodeBlockPrism } from "./tiptap_extensions/extension-code-block-refractor";
 import { CustomTable as Table } from "./tiptap_extensions/extension-table";
 
-const instants = new Map<string, HocuspocusProvider>();
-function getProvider(docName: string, ydoc: Doc) {
-  let provider = instants.get(docName);
-  if (!provider) {
-    provider = new HocuspocusProvider({
-      url: `ws://${window.location.host}/collaboration/${docName}`,
-      name: docName,
-      document: ydoc,
-      onAwarenessUpdate: ({ states }) => {
-        const customData = states.find((d) => d.data);
-        if (customData) {
-          // eslint-disable-next-line no-console
-          console.log(customData.data);
-        }
-      },
-    });
-    instants.set(docName, provider);
-  }
-  return provider;
-}
+// const instants = new Map<string, HocuspocusProvider>();
+// function getProvider(docName: string, ydoc: Doc) {
+//   let provider = instants.get(docName);
+//   if (!provider) {
+//     provider = new HocuspocusProvider({
+//       url: `ws://${window.location.host}/collaboration/${docName}`,
+//       name: docName,
+//       document: ydoc,
+//       onAwarenessUpdate: ({ states }) => {
+//         const customData = states.find((d) => d.data);
+//         if (customData) {
+//           // eslint-disable-next-line no-console
+//           console.log(customData.data);
+//         }
+//       },
+//     });
+//     instants.set(docName, provider);
+//   }
+//   return provider;
+// }
 
 // const CodeBlockHighlight = CodeBlockLowlight.configure({
 //   lowlight,
@@ -62,13 +62,13 @@ const CodeBlockPrismjs = CodeBlockPrism.configure({
 
 export {
   StarterKit,
-  Collaboration,
-  CollaborationCursor,
-  HocuspocusProvider,
+  // Collaboration,
+  // CollaborationCursor,
+  // HocuspocusProvider,
   // IndexeddbPersistence,
   UploadImageWithResizable,
-  getProvider,
-  Y,
+  // getProvider,
+  // Y,
   MarkdownPaste as markdownPasteExtension,
   Table,
   TableHeader,
