42845525451944481a12d9d7d8e4738b27b52933
fuzhongwei <jackyleefzw@163.com>
2024-04-22 11:10:51 +0800

    [monorepo] update lint check docs-site

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -58,6 +58,7 @@ module.exports = {
         "jsx-a11y/mouse-events-have-key-events": "off",
         "jsx-a11y/label-has-associated-control": "off",
         "jsx-a11y/no-noninteractive-element-interactions": "off",
+        "jsx-a11y/media-has-caption": "off",
       },
     },
 
