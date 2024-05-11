807f2de4097c5dd4d37cf06b77bea6fb50413c55
fuzhongwei <jackyleefzw@163.com>
2024-05-11 14:22:28 +0800

    [docs-site] SignaturePad add usage of custom style

---
+++ docs-site/app/src/demos/signaturepad/SignaturePadCustomDemo.tsx
@@ -0,0 +1,20 @@
+import { SignaturePad, SignaturePadHandle } from "@rtdui/signature-pad";
+import { useRef } from "react";
+
+export default function Demo() {
+  const signPadRef = useRef<SignaturePadHandle>(null!);
+
+  return (
+    <SignaturePad
+      ref={signPadRef}
+      slots={{
+        clearBtn:
+          "w-20 h-8 min-h-0 p-1 bg-red-500 text-white hover:bg-red-700 rounded-full",
+        confirmBtn:
+          "w-20 h-8 min-h-0 p-1 bg-blue-500 text-white  hover:bg-blue-700 rounded-full",
+      }}
+    />
+  );
+}
+
+Demo.displayName = "SignaturePadCustomDemo";
