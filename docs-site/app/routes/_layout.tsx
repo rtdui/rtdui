5500d1cab5b828b19f0c8e8a4832c9480ef66574
fuzhongwei <jackyleefzw@163.com>
2024-08-26 12:37:38 +0800

    [docs-site] update version and changelog

--- docs-site/app/routes/_layout.tsx
+++ docs-site/app/routes/_layout.tsx
@@ -248,7 +248,7 @@ export default function Layout() {
               RTD UI
             </a>
             <Link to="/changelog" className="link">
-              v4.8.0
+              v4.8.1
             </Link>
           </div>
           <NavMenu
