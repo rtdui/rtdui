1da874864f7b38d77e0f07840344d3d9a7c83d59
fuzhongwei <jackyleefzw@163.com>
2024-09-19 16:59:52 +0800

    [docs-site] add demo of DataTableSelect with filter

---
+++ docs-site/app/src/demos/datatableselect/DatatableSelectWithFilterDemo.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { DataTableSelect, type ColumnDef } from "@rtdui/datatable";
+import { IconAt } from "@tabler/icons-react";
+import { makePersonData, type Person } from "../../demoData/makeData";
+
+const dataTableColumns: ColumnDef<Person>[] = [
+  {
+    id: "姓名",
+    header: "姓名",
+    accessorKey: "fullName",
+    size: 260,
+    minSize: 260,
+    meta: {
+      expandable: true,
+    },
+  },
+];
+
+const getRowId = (row: any) => row.id;
+
+export default function Demo() {
+  const [data, setData] = React.useState<Person[]>([]);
+  React.useEffect(() => {
+    setData(makePersonData(20, 3));
+  }, []);
+
+  return (
+    <DataTableSelect
+      defaultValue="1"
+      placeholder="--请选择--"
+      displayField="fullName"
+      leftSection={<IconAt strokeWidth={1} size="1.2rem" />}
+      columns={dataTableColumns}
+      data={data}
+      getRowId={getRowId}
+      getSubRows={(row: any) => row.subRows}
+      enableFilters
+      filterFromLeafRows
+    />
+  );
+}
+Demo.displayName = "DatatableSelectBasicDemo";
