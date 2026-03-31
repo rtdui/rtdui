003643664f0f3f6426038a0122e4a8b051dcedc4
fuzhongwei <jackyleefzw@163.com>
2026-03-31 14:27:54 +0800

    [docs-site] 使用prerender

--- docs-site/react-router.config.ts
+++ docs-site/react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-	/* 
+  /* 
     prerender 属性指定 Static Pre-rendering, 有三种方式:
       // 1. all static route paths (no dynamic segments like "/post/:slug")
       prerender: true,
@@ -13,7 +13,8 @@ export default {
         return ["/", "/blog"].concat(posts.map((post) => post.href));
       }, 
   */
-	// prerender: true,
-	// serverBuildFile: "assets/server-build.js", // 🚨 使用Hono时启用预渲染别忘了这个
-	ssr: false, // 默认为true
+  // prerender: true,
+  // serverBuildFile: "assets/server-build.js", // 🚨 使用Hono时启用预渲染别忘了这个
+  ssr: false, // 默认为true
+  prerender: true,
 } satisfies Config;
