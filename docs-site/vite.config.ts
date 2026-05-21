dbd9a256e6e1be76debac81c3095ab3f53f031de
fuzhongwei <jackyleefzw@163.com>
2026-05-21 09:20:41 +0800

    [docs-site] 清理@tabler/icons-react v3.19 引入的dynamic import的历史遗留

--- docs-site/vite.config.ts
+++ docs-site/vite.config.ts
@@ -13,85 +13,78 @@ import rehypeSlug from "rehype-slug";
 import rehypeKatex from "rehype-katex";
 import rehypeShiki from "@shikijs/rehype";
 import {
-	transformerNotationDiff,
-	transformerNotationErrorLevel,
-	transformerNotationFocus,
-	transformerNotationHighlight,
-	transformerNotationWordHighlight,
-	// transformerRemoveLineBreak,
-	transformerRemoveNotationEscape,
-	transformerMetaHighlight,
-	transformerMetaWordHighlight,
-	transformerCompactLineOptions,
+  transformerNotationDiff,
+  transformerNotationErrorLevel,
+  transformerNotationFocus,
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+  // transformerRemoveLineBreak,
+  transformerRemoveNotationEscape,
+  transformerMetaHighlight,
+  transformerMetaWordHighlight,
+  transformerCompactLineOptions,
 } from "@shikijs/transformers";
 import { transformerColorizedBrackets } from "@shikijs/colorized-brackets";
 import type { ShikiTransformer } from "@shikijs/types";
 
 // 不要在vite.config.ts文件中引用项目中的包, 否则在cloudflare中构建会报错
 function transformerAddLangDataAttr(): ShikiTransformer {
-	return {
-		name: "@rtdui/shiki/transformers:add-lang-class",
-		pre(hast) {
-			hast.properties["data-language"] = this.options.lang;
-		},
-	};
+  return {
+    name: "@rtdui/shiki/transformers:add-lang-class",
+    pre(hast) {
+      hast.properties["data-language"] = this.options.lang;
+    },
+  };
 }
 
 const basicShikiTransformers = [
-	transformerNotationDiff(),
-	transformerNotationErrorLevel(),
-	transformerNotationFocus(),
-	transformerNotationHighlight(),
-	transformerNotationWordHighlight(),
-	// transformerRemoveLineBreak(),
-	transformerRemoveNotationEscape(),
-	transformerMetaHighlight(),
-	transformerMetaWordHighlight(),
-	transformerCompactLineOptions(),
-	transformerColorizedBrackets(),
-	transformerAddLangDataAttr(),
+  transformerNotationDiff(),
+  transformerNotationErrorLevel(),
+  transformerNotationFocus(),
+  transformerNotationHighlight(),
+  transformerNotationWordHighlight(),
+  // transformerRemoveLineBreak(),
+  transformerRemoveNotationEscape(),
+  transformerMetaHighlight(),
+  transformerMetaWordHighlight(),
+  transformerCompactLineOptions(),
+  transformerColorizedBrackets(),
+  transformerAddLangDataAttr(),
 ];
 
 export default defineConfig({
-	plugins: [
-		mdx({
-			remarkPlugins: [
-				remarkFrontmatter, // 只是让mdx识别markdown中的frontmatter
-				remarkMdxFrontmatter, // 必须依赖remarkFrontmatter, 将frontmatter metadata转换到mdx模块的export
-				remarkGfm,
-				remarkToc, // 指定特定的标题文本: toc或Table of contents或contents
-				remarkBreaks,
-				remarkMath,
-			],
-			rehypePlugins: [
-				[
-					rehypeShiki,
-					{
-						// themes: {
-						// 	light: "one-light",
-						// 	dark: "one-dark-pro",
-						// },
-						theme: "one-dark-pro",
-						transformers: basicShikiTransformers,
-					},
-				],
-				rehypeSlug, // 为Headings生成id特性
-				[rehypeKatex, { output: "html" }], // 只输出html,默认为"htmlAndMathml", 注意需要导入katex的css样式
-			],
-		}),
-		tailwindcss(),
-		reactRouter(),
-		tsconfigPaths({ projects: ["./tsconfig.json"] }),
-	],
-	ssr: {
-		noExternal: [/^qrcode.react/, /^@rtdui\/qr-code/],
-	},
-	server: { host: "0.0.0.0" },
-	// 临时解决@tabler/icons-react v3.19 引入的dynamic import导致Vite创建大量chunk
-	resolve: {
-		alias: {
-			// /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
-			"@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
-		},
-	},
+  plugins: [
+    mdx({
+      remarkPlugins: [
+        remarkFrontmatter, // 只是让mdx识别markdown中的frontmatter
+        remarkMdxFrontmatter, // 必须依赖remarkFrontmatter, 将frontmatter metadata转换到mdx模块的export
+        remarkGfm,
+        remarkToc, // 指定特定的标题文本: toc或Table of contents或contents
+        remarkBreaks,
+        remarkMath,
+      ],
+      rehypePlugins: [
+        [
+          rehypeShiki,
+          {
+            // themes: {
+            // 	light: "one-light",
+            // 	dark: "one-dark-pro",
+            // },
+            theme: "one-dark-pro",
+            transformers: basicShikiTransformers,
+          },
+        ],
+        rehypeSlug, // 为Headings生成id特性
+        [rehypeKatex, { output: "html" }], // 只输出html,默认为"htmlAndMathml", 注意需要导入katex的css样式
+      ],
+    }),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
+  ],
+  ssr: {
+    noExternal: [/^qrcode.react/, /^@rtdui\/qr-code/],
+  },
+  server: { host: "0.0.0.0" },
 });
